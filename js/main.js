f503098e8e2e061d37ef394500f3169f7178d784
KalyghniiA <45540208+KalyghniiA@users.noreply.github.com>
2021-05-29 15:27:28 +0500

    Задание 1

--- js/main.js
+++ js/main.js
@@ -18,19 +18,35 @@ const coordinates = [
 ];
 
 
-const getRandomNumber = ({startNumber = 0, endNumber = 1, symbolsAfterComma = 1}) => {
-  if(startNumber > endNumber) {
+const getRandomNumber = ({startNumber: start = 0, endNumber: end = 0}) => {
+  if(start > end) {
     return ReferenceError('Начальное число больше конечного');
   }
 
-  if(startNumber < 0 || endNumber < 0) {
+  if(start < 0 || end < 0) {
     return ReferenceError('Числа должны быть положительными');
   }
 
-  return (Math.random() * (endNumber - startNumber) + startNumber).toFixed(symbolsAfterComma);
+  return Math.floor(Math.random() * (end - start) + start);
 };
 
 
+const getRandomNumberWithDot = ({startNumber: start = 0, endNumber: end = 1, symbolsAfterComma = 1}) => {
+  if(start > end) {
+    return ReferenceError('Начальное число больше конечного');
+  }
+
+  if(start < 0 || end < 0) {
+    return ReferenceError('Числа должны быть положительными');
+  }
+
+  return (Math.random() * (end - start) + start).toFixed(symbolsAfterComma);
+};
+
+
+coordinates.forEach((el) => {
+  getRandomNumberWithDot(el);
+});
 
 coordinates.forEach((el) => {
   getRandomNumber(el);
