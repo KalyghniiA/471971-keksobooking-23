6c44e79dc1430e01b8fdad3b8862b924ce312229
Александр <kalyghnii@gmail.com>
2021-06-21 15:04:41 +0500

    Добавил пин управления

--- js/map.js
+++ js/map.js
@@ -1,7 +1,9 @@
 import { inputAddres } from './validation.js';
-
+const MAP_LAT = 35.652832;
+const MAP_LNG = 139.839478;
 const USER_MARKER_LAT = 35.65952;
 const USER_MARKER_LNG = 139.78179;
+const ZOOM_MAP = 12;
 
 
 const USER_MARKER_SETTING = {
@@ -10,7 +12,7 @@ const USER_MARKER_SETTING = {
   anchor: [26, 52],
 };
 inputAddres.value = `${USER_MARKER_LAT}, ${USER_MARKER_LNG}`;
-const mymap = L.map('mapid').setView([35.652832, 139.839478], 12);
+const myMap = L.map('mapid').setView([MAP_LAT, MAP_LNG], ZOOM_MAP);
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWthbHlnaG5paSIsImEiOiJja3BtaGdyYzIwZmh5Mm9tZXVuamZ6cGJvIn0.BCZrDM9bJwrbOYzVqxDZjw', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 12,
@@ -18,7 +20,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{
   tileSize: 512,
   zoomOffset: -1,
   accessToken: 'your.mapbox.access.token',
-}).addTo(mymap);
+}).addTo(myMap);
 
 const userMarkerIcon = L.icon({
   iconUrl: USER_MARKER_SETTING.url,
@@ -26,11 +28,11 @@ const userMarkerIcon = L.icon({
   iconAnchor: USER_MARKER_SETTING.anchor,
 });
 
-const userMarker = L.marker([USER_MARKER_LAT, USER_MARKER_LNG], {icon: userMarkerIcon, draggable: true}).addTo(mymap);
+const userMarker = L.marker([USER_MARKER_LAT, USER_MARKER_LNG], {icon: userMarkerIcon, draggable: true}).addTo(myMap);
 
 userMarker.on('moveend', (evt) => {
   const {lat, lng} = evt.target.getLatLng();
   inputAddres.value = `${lat}, ${lng}`;
 });
 
-export {mymap};
+export {myMap};
